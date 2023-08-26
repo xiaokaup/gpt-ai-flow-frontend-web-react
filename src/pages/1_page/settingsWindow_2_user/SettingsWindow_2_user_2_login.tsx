c3dfd63d5a8b3d567c19f8b2f796a32ca575d229
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-26 23:14:51 +0200

    Move pagse

---
+++ src/pages/1_page/settingsWindow_2_user/SettingsWindow_2_user_2_login.tsx
@@ -0,0 +1,149 @@
+import "../../../styles/global.css";
+
+import React from "react";
+
+import { Button, Form, Input, message } from "antd";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
+import { IUserDB } from "../../../gpt-ai-flow-common/interface-database/IUserDB";
+// import { STORE_USER } from "../../../../tools/4_base/TConstant";
+import { EUserPageCase } from ".";
+
+interface ISettingsWindow_2_user_2_login_input {
+  setPageCase: (paraPageCase: EUserPageCase) => void;
+}
+export const SettingsWindow_2_user_2_login = (
+  props: ISettingsWindow_2_user_2_login_input
+) => {
+  const { setPageCase } = props;
+
+  const onEmailAndPasswordSignInFinish = async (values: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      // const userAndTokenData: IUserDB =
+      //   await TSettingsWindow_2_user.authLoginByEmailAndPassword(
+      //     values.email,
+      //     values.password,
+      //     window.env
+      //   );
+
+      // if (!userAndTokenData) {
+      //   throw new Error(
+      //     "用户的邮箱未被注册在或密码错误，如果多次有问题，请联系管理员"
+      //   );
+      // }
+
+      // Set userInfo in store
+      // window.electron.store.set(STORE_USER, userAndTokenData);
+      setPageCase(EUserPageCase.INFO);
+    } catch (error: any) {
+      message.error({
+        content: error?.message,
+        key: "auth",
+        duration: 3,
+      });
+    }
+  };
+
+  const onEmailAndPasswordSignInFaild = (errorInfo: any) => {
+    console.log("失败:", errorInfo);
+  };
+
+  return (
+    <div
+      className="row"
+      style={{
+        marginTop: 12,
+        marginLeft: 12,
+        // display: 'flex',
+        // flexDirection: 'column',
+        // alignItems: 'center',
+      }}
+    >
+      <div className="block_email_and_password">
+        <Form
+          name="normal_login"
+          className="login-form"
+          layout="horizontal"
+          initialValues={{ remember: true }}
+          onFinish={onEmailAndPasswordSignInFinish}
+          onFinishFailed={onEmailAndPasswordSignInFaild}
+          style={{ width: 300 }}
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "请输入你的邮箱",
+              },
+              {
+                type: "email",
+                message: "请以正确的格式输入",
+              },
+            ]}
+          >
+            <Input prefix={<MailOutlined />} placeholder={"邮箱"} />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "请输入你的密码",
+              },
+            ]}
+          >
+            <Input.Password prefix={<LockOutlined />} placeholder={"密码"} />
+          </Form.Item>
+
+          <Form.Item>
+            <div>
+              <Button
+                className="login_button login_button_with_password_provider"
+                type="primary"
+                htmlType="submit"
+              >
+                登陆
+              </Button>
+              <span style={{ marginLeft: 20 }}>
+                <Button
+                  type="default"
+                  onClick={() => {
+                    setPageCase(EUserPageCase.SIGNUP);
+                  }}
+                >
+                  注册
+                </Button>
+              </span>
+              <br />
+              <span
+                style={{ marginLeft: 4, color: "#7C7C7C", cursor: "pointer" }}
+                onClick={() => setPageCase(EUserPageCase.FORGET_PASSWORD)}
+              >
+                忘记密码
+              </span>
+            </div>
+
+            {/* <div>
+              <a href="/user/forgetpassword">
+                <Button
+                  type="text"
+                  size="small"
+                  style={{
+                    padding: 0,
+                    // color: color_gray
+                  }}
+                >
+                  Forgot password
+                </Button>
+              </a>
+            </div> */}
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/1_page/settingsWindow_2_user/SettingsWindow_2_user_3_info.tsx
@@ -0,0 +1,101 @@
+import "../../../styles/global.css";
+import "../../../styles/layout.scss";
+
+import React, { useState } from "react";
+
+import { Button, Descriptions } from "antd";
+import { EUserPageCase } from ".";
+import IUserDBFile, {
+  IUserDB_default,
+} from "../../../gpt-ai-flow-common/interface-database/IUserDB";
+import { IUserData } from "../../../gpt-ai-flow-common/interface-app/IUserData";
+// import { STORE_USER } from "../../../tools/4_base/TConstant";
+// import { useUserInfo } from "../../../hooks/useUserInfo";
+
+interface ISettingsWindow_2_user_3_info_input {
+  setPageCase: (paraPageCase: EUserPageCase) => void;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
+}
+export const SettingsWindow_2_user_3_info = (
+  props: ISettingsWindow_2_user_3_info_input
+) => {
+  // const { userData } = useUserInfo();
+  const [userData, setUserData] = useState<IUserData>(
+    IUserDBFile.IUserDB_default
+  );
+
+  const { setPageCase, setIsAuthenticated } = props;
+
+  return (
+    <div
+      className="row"
+      style={{
+        marginTop: 12,
+        marginLeft: 12,
+        // display: 'flex',
+        // flexDirection: 'column',
+        // alignItems: 'center',
+      }}
+    >
+      {/* <div>
+        <pre>{JSON.stringify(userData, null, 2)}</pre>
+      </div> */}
+
+      <div className="row block_user_info">
+        <Descriptions
+          title={"个人资料"}
+          // layout="vertical"
+          // column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+        >
+          {/* <Descriptions.Item label={t('Account type')}>
+            {internalRoles?.join(',') ?? 'User'}
+          </Descriptions.Item> */}
+          <Descriptions.Item label={"姓"}>
+            {userData.firstName}
+          </Descriptions.Item>
+          <Descriptions.Item label={"名字"}>
+            {userData.lastName}
+          </Descriptions.Item>
+          <Descriptions.Item label={"昵称"}>
+            {userData.displayName}
+          </Descriptions.Item>
+          <Descriptions.Item label={"邮箱"}>{userData.email}</Descriptions.Item>
+          {(userData.userRoles ?? []).length > 0 && (
+            <Descriptions.Item label="角色" span={3}>
+              {(userData.userRoles ?? []).join(", ")}
+            </Descriptions.Item>
+          )}
+          {(userData.userRolePermissions ?? []).length > 0 && (
+            <Descriptions.Item label="权限" span={3}>
+              {(userData.userRolePermissions ?? [])
+                .map((item) => item.replace(/-build-in/g, ""))
+                .join(", ")}
+            </Descriptions.Item>
+          )}
+        </Descriptions>
+      </div>
+
+      <div className="row">
+        <Button onClick={() => setPageCase(EUserPageCase.CHANGE_PASSWORD)}>
+          修改密码
+        </Button>
+      </div>
+
+      <div className="row">
+        <Button
+          type="link"
+          onClick={() => {
+            // window.electron.store.set(STORE_USER, IUserDB_default);
+            setIsAuthenticated(false);
+            setTimeout(() => {
+              setPageCase(EUserPageCase.LOGIN);
+            }, 1000);
+          }}
+          style={{ cursor: "pointer", marginTop: 10, marginBottom: 14 }}
+        >
+          登出
+        </Button>
+      </div>
+    </div>
+  );
+};
