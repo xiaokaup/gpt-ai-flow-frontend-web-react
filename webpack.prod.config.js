84c342f0aaba68178d803223eb85e7f76835b9d9
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-27 11:10:42 +0200

    up

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,10 @@ const { merge } = require('webpack-merge');
 const base = require('./webpack.config.js');
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(base, {
