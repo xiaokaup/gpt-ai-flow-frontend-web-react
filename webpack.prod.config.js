ba88b644b1c1f0416f9de083eabab9711b74db61
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-27 11:06:13 +0200

    IUP

---
+++ webpack.prod.config.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const base = require('./webpack.config.js');
+const path = require('path');
+
+const Dotenv = require('dotenv-webpack');
+
+module.exports = merge(base, {
+  mode: 'production',
+  plugins: [
+    new Dotenv({
+      path: './.env.prod',
+      systemvars: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false, // 如果你需要 source map，请将此设置为 true
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+});
