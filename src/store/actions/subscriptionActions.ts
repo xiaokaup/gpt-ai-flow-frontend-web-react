47ae9cefc67f40f5d5b8151aa792f97e04d99186
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-16 19:14:41 +0200

    Create src/hooks/useUserSubscriptionInfo.ts

--- src/store/actions/subscriptionActions.ts
+++ src/store/actions/subscriptionActions.ts
@@ -1,10 +1,11 @@
 import { IConstantGptAiFlowHandler } from '../../gpt-ai-flow-common/config/constantGptAiFlow';
+import { ISubscirptionMix } from '../../gpt-ai-flow-common/interface-app/3_unit/ISubscriptionMix';
 import TBackendSubscriptionFile from '../../tools/3_unit/TBackendSubscription';
 
 export const GET_SUBSCRIPTION_INFO = 'GET_SUBSCRIPTION_INFO';
 export const getSubscriptionInfoAction =
   (userId: string, accessToken: string, env: IConstantGptAiFlowHandler) => async (dispatch: any) => {
-    const results = await TBackendSubscriptionFile.getSubscriptionInfo(userId, accessToken, env);
+    const results: ISubscirptionMix = await TBackendSubscriptionFile.getSubscriptionInfo(userId, accessToken, env);
 
     dispatch({ type: GET_SUBSCRIPTION_INFO, payload: results });
 
