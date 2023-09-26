5a807981f53a3d40d92edd189de4ce1fda8cc7fb
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-26 22:31:17 +0200

    Resetup config for typescript and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+  },
+  plugins: ['@typescript-eslint', 'unused-imports'],
+};
