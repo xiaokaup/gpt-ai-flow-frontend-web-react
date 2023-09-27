40e8b288590b483272970ac7b049e4d2ee0a5c86
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-09-27 18:03:18 +0200

    udpate

--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
-import { ICounterReducerState, counterReducer } from './CounterReducer';
+
 import { IUserData } from '../../gpt-ai-flow-common/interface-app/IUserData';
+import { ISubscirptionMix } from '../../gpt-ai-flow-common/interface-app/3_unit/ISubscriptionMix';
+import { IInputsCache } from '../../gpt-ai-flow-common/interface-app/3_unit/IInputsCache';
+
+import { ICounterReducerState, counterReducer } from './CounterReducer';
 import { userReducer } from './userReducer';
 import { ILocalReducerState, localReducer } from './localReducer';
 import { proModeReducer } from './proModeReducer';
-import { ISubscirptionMix } from '../../gpt-ai-flow-common/interface-app/3_unit/ISubscriptionMix';
 import { subscriptionReducer } from './SubscriptionReducer';
-import { IInputsCache, inputsCacheReduer } from './inputsCacheReducer';
+import { inputsCacheReduer } from './inputsCacheReducer';
 
 export interface IReduxRootState {
   counterInfo: ICounterReducerState;
