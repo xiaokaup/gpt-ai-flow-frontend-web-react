d91a6357e228324c9570af1447bc43418277629c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-10-16 12:17:57 +0200

    update v

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -71,8 +71,8 @@ export const AppRoutes = () => {
         <AppLayout isAuthenticated={isAuthenticated}>
           <SettingsWindow_2_user_3_info userData={userData} isAuthenticated={isAuthenticated} />
           <SettingsWindow_1_local />
-          <SettingsWindow_4_proMode />
           <SettingsWindow_6_about />
+          <SettingsWindow_4_proMode />
         </AppLayout>
       ),
     },
