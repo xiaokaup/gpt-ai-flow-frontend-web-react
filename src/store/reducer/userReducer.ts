566914a9848a5b2cec6cb81b2f451a006c7028b7
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-10-23 12:34:37 +0200

    Solution 1

--- src/store/reducer/userReducer.ts
+++ src/store/reducer/userReducer.ts
@@ -5,6 +5,7 @@ import {
   USER_LOGOUT,
   USER_SIGN_UP,
   USER_UPDATE_USER_PASSWORD_V1,
+  UPDATE_USER_ROLES_AND_USER_PERMISSIONS,
 } from '../actions/userActions';
 import { IAction } from '../store';
 
@@ -16,6 +17,11 @@ export const userReducer = (state: IUserReducerState = IUserDataFile.IUserData_d
   switch (type) {
     case USER_LOGIN:
       return payload;
+    case UPDATE_USER_ROLES_AND_USER_PERMISSIONS:
+      if (state.userRoles.length === payload.userRoles?.length) return state;
+      if (state.userRolePermissions.length === payload.userRolePermissions?.length) return state;
+
+      return { ...state, ...payload };
     case USER_LOGOUT:
       return IUserDataFile.IUserData_default;
     case USER_UPDATE_USER_PASSWORD_V1:
