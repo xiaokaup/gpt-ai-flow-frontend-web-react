7ea89a36b6426f7ae3d330246c9e2ac2a81cb3f4
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-10-24 23:11:11 +0200

    Simplify codes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import CONSTANTS_GPT_AI_FLOW_COMMON from './gpt-ai-flow-common/config/constantGptAiFlow';
 import { useUserData } from './gpt-ai-flow-common/hooks/useUserData';
@@ -32,83 +32,86 @@ export const AppRoutes = () => {
     env: CONSTANTS_GPT_AI_FLOW_COMMON,
   });
 
-  const router = createBrowserRouter([
-    // {
-    //   path: '/counter',
-    //   element: (
-    //     <AppLayout isAuthenticated={isAuthenticated}>
-    //       <CounterComponent />
-    //     </AppLayout>
-    //   ),
-    // },
-    {
-      path: '/',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <SettingsWindow_2_user_2_login />
-        </AppLayoutCenter>
-      ),
-    },
-    {
-      path: '/signUp',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <SettingsWindow_2_user_1_signup />
-        </AppLayoutCenter>
-      ),
-    },
-    {
-      path: '/login',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <SettingsWindow_2_user_2_login />
-        </AppLayoutCenter>
-      ),
-    },
-    {
-      path: '/info',
-      element: (
-        <AppLayout isAuthenticated={isAuthenticated}>
-          <SettingsWindow_2_user_3_info userData={userData} isAuthenticated={isAuthenticated} />
-          <SettingsWindow_1_local />
-          <SettingsWindow_6_about />
-          <SettingsWindow_4_proMode />
-        </AppLayout>
-      ),
-    },
-    {
-      path: '/changePassword',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <SettingsWindow_2_user_4_changePassword userData={userData} isAuthenticated={isAuthenticated} />
-        </AppLayoutCenter>
-      ),
-    },
-    {
-      path: '/forgetPassword',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <SettingsWindow_2_user_5_forgetPassword />
-        </AppLayoutCenter>
-      ),
-    },
-    {
-      path: '/proMode',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <ProModeWindow />
-        </AppLayoutCenter>
-      ),
-    },
-    {
-      path: '/logout',
-      element: (
-        <AppLayoutCenter isAuthenticated={isAuthenticated}>
-          <LogoutPage />
-        </AppLayoutCenter>
-      ),
-    },
-  ]);
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route
+          path={"${globalUrlPrefix}/counter"}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <CounterComponent />
+            </AppLayoutCenter>
+          }
+        /> */}
 
-  return <RouterProvider router={router} />;
+        <Route
+          path={'/'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <SettingsWindow_2_user_2_login />
+            </AppLayoutCenter>
+          }
+        />
+        <Route
+          path={'/signUp'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <SettingsWindow_2_user_1_signup />
+            </AppLayoutCenter>
+          }
+        />
+        <Route
+          path={'/login'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <SettingsWindow_2_user_2_login />
+            </AppLayoutCenter>
+          }
+        />
+        <Route
+          path={'/info'}
+          element={
+            <AppLayout isAuthenticated={isAuthenticated}>
+              <SettingsWindow_2_user_3_info userData={userData} isAuthenticated={isAuthenticated} />
+              <SettingsWindow_1_local />
+              <SettingsWindow_6_about />
+              <SettingsWindow_4_proMode />
+            </AppLayout>
+          }
+        />
+        <Route
+          path={'/changePassword'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <SettingsWindow_2_user_4_changePassword userData={userData} isAuthenticated={isAuthenticated} />
+            </AppLayoutCenter>
+          }
+        />
+        <Route
+          path={'/forgetPassword'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <SettingsWindow_2_user_5_forgetPassword />
+            </AppLayoutCenter>
+          }
+        />
+        <Route
+          path={'/proMode'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <ProModeWindow />
+            </AppLayoutCenter>
+          }
+        />
+        <Route
+          path={'/logout'}
+          element={
+            <AppLayoutCenter isAuthenticated={isAuthenticated}>
+              <LogoutPage />
+            </AppLayoutCenter>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
