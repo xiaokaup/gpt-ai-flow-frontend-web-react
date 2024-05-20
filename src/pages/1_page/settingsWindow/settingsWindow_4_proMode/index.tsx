dbeb00cd64c3d0da6ce0e2d74fee78b6619094a7
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-20 18:19:05 +0200

    Update texts

--- src/pages/1_page/settingsWindow/settingsWindow_4_proMode/index.tsx
+++ src/pages/1_page/settingsWindow/settingsWindow_4_proMode/index.tsx
@@ -160,7 +160,7 @@ const SettingsWindow_4_proMode_login = (props: ISettingsWindow_4_proMode_login_i
                   }
                   onClick={() => setTabSelected('Tool')}
                 >
-                  {t.get('Tool_version')} ({t.get('Requires your own OpenAI key')})
+                  {t.get('Tools_version')} ({t.get('Requires your own OpenAI key')})
                 </a>
               </div>
             </div>
@@ -295,7 +295,7 @@ const SettingsWindow_4_proMode_login = (props: ISettingsWindow_4_proMode_login_i
                 <div className="pricing-table paricing-table-tool !mt-12 container space-y-12 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8">
                   <div className="relative p-8  border border-gray-200 rounded-2xl shadow-sm flex flex-col">
                     <div className="flex-1">
-                      <h3 className="text-xl font-semibold ">{t.get('Tool_version')}</h3>
+                      <h3 className="text-xl font-semibold ">{t.get('Tools_version')}</h3>
                       <p className="mt-4 flex items-baseline ">
                         <span className="text-5xl font-extrabold tracking-tight">
                           {locale_for_currency === ELocale.EN ? '$4.95' : '￥34.95'}
