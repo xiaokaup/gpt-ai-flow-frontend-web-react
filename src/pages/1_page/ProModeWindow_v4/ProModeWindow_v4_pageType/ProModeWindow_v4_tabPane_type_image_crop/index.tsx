58a91c2565b805579a146272954c6e8eb5687b98
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-21 19:00:56 +0200

    Create src/pages/1_page/ProModeWindow_v4/ProModeWindow_v4_pageType/ProModeWindow_v4_tabPane_type_image_crop/index.tsx

---
+++ src/pages/1_page/ProModeWindow_v4/ProModeWindow_v4_pageType/ProModeWindow_v4_tabPane_type_image_crop/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ProModeWindow_v4_tabPane_type_image_crop = () => {
+  return (
+    <div className="page_container" style={{ maxWidth: 'unset' }}>
+      ProModeWindow_v4_tabPane_type_image_crop
+    </div>
+  );
+};
