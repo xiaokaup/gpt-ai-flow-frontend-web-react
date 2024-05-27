4c809729e594d346beeb6283a55282e1f41250cb
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-27 11:56:31 +0200

    aa

--- src/pages/1_page/NewsPage/index.tsx
+++ src/pages/1_page/NewsPage/index.tsx
@@ -37,11 +37,11 @@ export const NewsPage = (props: INewsPageProps) => {
 
   return (
     <div className="drag-region" style={{ width: '100%' }}>
-      <div className="container newsPage">
+      <div className="container newsPage my-4">
         <div className="row top_block">
           <h1>News</h1>
           {news === undefined && <p>Loading...</p>}
-          {news && (
+          {/* {news && (
             <ul>
               <li>toutiao-search: {(news['toutiao-search'] as any[]).length}</li>
               <li>weibo-search: {(news['weibo-search'] as any[]).length}</li>
@@ -49,37 +49,37 @@ export const NewsPage = (props: INewsPageProps) => {
               <li>zhihu-search: {(news['zhihu-search'] as any[]).length}</li>
               <li>zhihu-video: {(news['zhihu-video'] as any[]).length}</li>
             </ul>
-          )}
+          )} */}
         </div>
 
         {news && (
-          <div className="news_block" style={{ display: 'flex', flexWrap: 'wrap' }}>
+          <div className="news_block mb-10" style={{ display: 'flex', flexWrap: 'wrap' }}>
             {news['toutiao-search'] && (
-              <div className="column" style={{ flex: '0 1 31%' }}>
+              <div style={{ flex: '0 1 32%', marginLeft: '1rem' }}>
                 <ToutiaoSearchCard news={news['toutiao-search']} />
               </div>
             )}
 
             {news['weibo-search'] && (
-              <div className="column" style={{ flex: '0 1 31%' }}>
+              <div style={{ flex: '0 1 32%', marginLeft: '1rem' }}>
                 <WeiboSearchCard news={news['weibo-search']} />
               </div>
             )}
 
             {news['zhihu-questions'] && (
-              <div className="column" style={{ flex: '0 1 31%' }}>
+              <div style={{ flex: '0 1 32%', marginLeft: '1rem' }}>
                 <ZhihuQuestions news={news['zhihu-questions']} />
               </div>
             )}
 
             {news['zhihu-search'] && (
-              <div className="column" style={{ flex: '0 1 31%' }}>
+              <div style={{ flex: '0 1 32%', marginLeft: '1rem' }}>
                 <ZhihuSearch news={news['zhihu-search']} />
               </div>
             )}
 
             {news['zhihu-video'] && (
-              <div className="column" style={{ flex: '0 1 31%' }}>
+              <div style={{ flex: '0 1 32%', marginLeft: '1rem' }}>
                 <ZhihuVideo news={news['zhihu-video']} />
               </div>
             )}
