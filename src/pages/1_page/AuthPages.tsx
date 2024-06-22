acbcfd647d71c40fccf5e3146e3f72fece19c6b1
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-06-22 20:39:35 +0200

    Handle openAIApiKey in authPage

--- src/pages/1_page/AuthPages.tsx
+++ src/pages/1_page/AuthPages.tsx
@@ -1,12 +1,20 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-import { IGetT_frontend_output } from '../../gpt-ai-flow-common/i18nProvider/ILocalesFactory';
-import { useLocation } from 'react-router-dom';
-import { IUserData } from '../../gpt-ai-flow-common/interface-app/3_unit/IUserData';
-import { getUser } from '../../gpt-ai-flow-common/tools/3_unit/TBackendUser';
+import { message } from 'antd';
+
+import { IReduxRootState } from '../../store/reducer';
 import { USER_LOGIN } from '../../store/actions/userActions';
+import { saveLocalAction } from '../../store/actions/localActions';
+
+import {
+  IStoreStorageLocalSettings,
+  IStoreStorageLocalSettings_default,
+} from '../../gpt-ai-flow-common/interface-app/4_base/IStoreStorage';
+import { getUser } from '../../gpt-ai-flow-common/tools/3_unit/TBackendUser';
+import { IUserData } from '../../gpt-ai-flow-common/interface-app/3_unit/IUserData';
+import { IGetT_frontend_output } from '../../gpt-ai-flow-common/i18nProvider/ILocalesFactory';
 import CONSTANTS_GPT_AI_FLOW_COMMON from '../../gpt-ai-flow-common/config/constantGptAiFlow';
 
 interface IAuthPage_input {
@@ -18,14 +26,35 @@ export const AuthPage = (props: IAuthPage_input) => {
 
   const { t } = props;
 
+  const localFromStore: IStoreStorageLocalSettings = useSelector((state: IReduxRootState) => {
+    return state.local ?? IStoreStorageLocalSettings_default;
+  });
+
   const query = new URLSearchParams(useLocation().search);
   const userId = query.get('id');
   const accessToken = query.get('accessToken');
+  const openAIApiKey = query.get('openAIApiKey');
 
   const init = async () => {
     const userDataFound: IUserData = await getUser(userId, accessToken, t.currentLocale, CONSTANTS_GPT_AI_FLOW_COMMON);
     console.log('userDataFound', userDataFound);
+
+    if (!userDataFound) {
+      message.error(
+        t.get(
+          "Dear user, you must log in to your account to enjoy all the features of this software.\n\nIf you have already logged in but are still experiencing some issues, don't worry, try restarting the software to see if that helps.",
+        ),
+      );
+      return;
+    }
+
     dispatch({ type: USER_LOGIN, payload: userDataFound });
+    dispatch<IStoreStorageLocalSettings | any>(
+      saveLocalAction({
+        ...localFromStore,
+        openAIApiKey: openAIApiKey.trim(),
+      }),
+    );
     setTimeout(() => {
       navigate('/proMode');
       window.location.reload();
@@ -41,5 +70,5 @@ export const AuthPage = (props: IAuthPage_input) => {
     }
   }, [userId, accessToken]);
 
-  return <div>{t.get('Auth')}...</div>;
+  return <div className="p-20">{t.get('Auth')}...</div>;
 };
