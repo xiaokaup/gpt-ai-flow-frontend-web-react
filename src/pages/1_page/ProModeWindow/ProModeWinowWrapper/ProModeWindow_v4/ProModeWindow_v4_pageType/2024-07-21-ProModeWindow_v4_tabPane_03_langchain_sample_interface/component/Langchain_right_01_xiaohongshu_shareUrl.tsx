1c620f6ae9ef3c277fc7ae08bfc580c0f5b32a9a
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-07-30 23:25:41 +0200

    Find souceUrl from shareUrl

--- src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2024-07-21-ProModeWindow_v4_tabPane_03_langchain_sample_interface/component/Langchain_right_01_xiaohongshu_shareUrl.tsx
+++ src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2024-07-21-ProModeWindow_v4_tabPane_03_langchain_sample_interface/component/Langchain_right_01_xiaohongshu_shareUrl.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import { Button, Form, Input } from 'antd';
+import iconSuccessful from '../../../../../../../../../assets/icons-customize/icon-status-successful/icon-status-successful-512x512.png';
+import iconWrong from '../../../../../../../../../assets/icons-customize/icon-status-wrong/icon-status-wrong-512x512.png';
 import TBackendPuppeteerFile from '../../../../../../../../gpt-ai-flow-common/tools/3_unit/TBackendPuppeteer';
 import { ELocale } from '../../../../../../../../gpt-ai-flow-common/enum-app/ELocale';
 import CONSTANTS_GPT_AI_FLOW_COMMON from '../../../../../../../../gpt-ai-flow-common/config/constantGptAiFlow';
+import { IInternalRequest } from '../../../../../../../../gpt-ai-flow-common/interface-app/3_unit/IRequest';
 
 const { Search } = Input;
 
 export const Langchain_right_01_xiaohongshu_shareUrl = () => {
   const [xiaohongshu_shareUrl, setXiaohongshu_shareUrl] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
+  const [hasAnalyseRequests, setHasAnalyseRequests] = useState<boolean>(false);
+  const [hasSourceUrl, setHasSourceUrl] = useState<boolean>(false);
 
-  const [analyseRequests, setAnalyseRequests] = useState<any[]>([]);
+  const [analyseRequests, setAnalyseRequests] = useState<IInternalRequest[]>([]);
 
   const analyseRequests_for_shareUrl = async (shareUrl: string) => {
+    setHasAnalyseRequests(true);
+
     const results = await TBackendPuppeteerFile.getAnalyseRequests_for_shareUrl_by_backend(
       shareUrl,
       ELocale.ZH,
@@ -21,6 +28,13 @@ export const Langchain_right_01_xiaohongshu_shareUrl = () => {
 
     console.log('results: ', results);
     setAnalyseRequests(results);
+
+    const filterUrl = results.find((item: IInternalRequest) => {
+      return item.url.startsWith('https://www.xiaohongshu.com/discovery/item');
+    });
+
+    setHasSourceUrl(filterUrl);
+    console.log('filterUrl', filterUrl);
   };
 
   return (
@@ -58,6 +72,16 @@ export const Langchain_right_01_xiaohongshu_shareUrl = () => {
               await analyseRequests_for_shareUrl(xiaohongshu_shareUrl);
               setLoading(false);
             }}
+            suffix={
+              <>
+                {hasAnalyseRequests && !loading && hasSourceUrl && (
+                  <img src={iconSuccessful} alt="" style={{ width: 18, marginLeft: '.2rem', marginRight: '.2rem' }} />
+                )}
+                {hasAnalyseRequests && !loading && !hasSourceUrl && (
+                  <img src={iconWrong} alt="" style={{ width: 18, marginLeft: '.4rem' }} />
+                )}
+              </>
+            }
           />
         </Form.Item>
       </div>
