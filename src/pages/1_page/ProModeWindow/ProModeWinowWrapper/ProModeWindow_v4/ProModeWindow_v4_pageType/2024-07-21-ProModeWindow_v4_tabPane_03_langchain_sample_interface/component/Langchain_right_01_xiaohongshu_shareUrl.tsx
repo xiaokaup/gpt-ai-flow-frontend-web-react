be4e8ad8407d39ed71d3c4893563fb4f5f08d062
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-07-30 22:09:11 +0200

    Can anlayse shareUrl from xiaohongshu

--- src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2024-07-21-ProModeWindow_v4_tabPane_03_langchain_sample_interface/component/Langchain_right_01_xiaohongshu_shareUrl.tsx
+++ src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2024-07-21-ProModeWindow_v4_tabPane_03_langchain_sample_interface/component/Langchain_right_01_xiaohongshu_shareUrl.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
-import { Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
+import TBackendPuppeteerFile from '../../../../../../../../gpt-ai-flow-common/tools/3_unit/TBackendPuppeteer';
+import { ELocale } from '../../../../../../../../gpt-ai-flow-common/enum-app/ELocale';
+import CONSTANTS_GPT_AI_FLOW_COMMON from '../../../../../../../../gpt-ai-flow-common/config/constantGptAiFlow';
 
 const { Search } = Input;
 
@@ -7,21 +10,57 @@ export const Langchain_right_01_xiaohongshu_shareUrl = () => {
   const [xiaohongshu_shareUrl, setXiaohongshu_shareUrl] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
+  const [analyseRequests, setAnalyseRequests] = useState<any[]>([]);
+
+  const analyseRequests_for_shareUrl = async (shareUrl: string) => {
+    const results = await TBackendPuppeteerFile.getAnalyseRequests_for_shareUrl_by_backend(
+      shareUrl,
+      ELocale.ZH,
+      CONSTANTS_GPT_AI_FLOW_COMMON,
+    );
+
+    console.log('results: ', results);
+    setAnalyseRequests(results);
+  };
+
   return (
     <>
-      <Form.Item name={'xiaohongshu_shareUrl'} label={'xiaohognshu shareUrl'} rules={[]}>
-        <Search
-          value={xiaohongshu_shareUrl}
-          onChange={(event) => {
-            setXiaohongshu_shareUrl(event.target.value);
+      <div className="row">
+        <Button
+          onClick={() => {
+            console.log('xiaohongshu_shareUrl: ', xiaohongshu_shareUrl);
           }}
-          loading={loading}
-          onSearch={(value) => {
-            console.log('onSearch value', value);
-            setLoading(true);
+        >
+          xiaohongshu_shareUrl
+        </Button>
+        <Button
+          onClick={() => {
+            console.log('analyseRequests: ', analyseRequests);
           }}
-        />
-      </Form.Item>
+        >
+          analyseRequests
+        </Button>
+      </div>
+
+      <div className="row">
+        <Form.Item name={'xiaohongshu_shareUrl'} label={'xiaohognshu shareUrl'} rules={[]}>
+          <Search
+            value={xiaohongshu_shareUrl}
+            onChange={(event) => {
+              if (!event.target.value) return;
+              setXiaohongshu_shareUrl(event.target.value);
+            }}
+            loading={loading}
+            enterButton={loading}
+            onSearch={async (value) => {
+              console.log('onSearch value', value);
+              setLoading(true);
+              await analyseRequests_for_shareUrl(xiaohongshu_shareUrl);
+              setLoading(false);
+            }}
+          />
+        </Form.Item>
+      </div>
     </>
   );
 };
