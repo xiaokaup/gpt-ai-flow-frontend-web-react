d1163d4d4132fc01f47e5bf1d3b365f3ec42627e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-08-06 12:08:28 +0200

    Upgrade from v0.1.67 to v0.1.68

--- src/pages/1_page/settingsWindow/SettingsWindow_7_about.tsx
+++ src/pages/1_page/settingsWindow/SettingsWindow_7_about.tsx
@@ -30,7 +30,7 @@ export const SettingsWindow_7_about = (props: SettingsWindow_7_about_input) => {
 
       <hr />
       <div className="row">
-        <span>{t.get('Release version')}: v0.1.67</span>
+        <span>{t.get('Release version')}: v0.1.68</span>
       </div>
     </div>
   );
