0483836a5e4c882d3c0ddb0b53b2452712df000c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-10-03 23:59:07 +0200

    Add tooltips into reactFlows

--- src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v5/features/01-ReactFlow.tsx
+++ src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v5/features/01-ReactFlow.tsx
@@ -1,5 +1,14 @@
 import { useCallback } from 'react';
-import { ReactFlow, useNodesState, useEdgesState, addEdge } from '@xyflow/react';
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+} from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 
@@ -24,14 +33,20 @@ export default function App() {
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: '100vw', height: '60vh' }}>
+      {/* <div style={{ width: '100vw', height: '100vh' }}> */}
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
-      />
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+      {/* </div> */}
     </div>
   );
 }
