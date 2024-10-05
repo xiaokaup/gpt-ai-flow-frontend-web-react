8e112d241797608cda5a7c2eb1c2390371e2710c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-10-05 16:04:25 +0200

    Clean codes

--- src/pages/1_page/ProModeWindow/ProModeWinowWrapper/index.tsx
+++ src/pages/1_page/ProModeWindow/ProModeWinowWrapper/index.tsx
@@ -18,7 +18,7 @@ export const ProModeWindow_warpper = (props: IProModeWindow_warpper) => {
   const { t, locale } = props.webCase;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [version, setVersion] = useState<string>('v4_v2');
+  const [version, setVersion] = useState<string>('v4');
 
   return (
     <div style={{ width: '100%' }}>
@@ -36,10 +36,10 @@ export const ProModeWindow_warpper = (props: IProModeWindow_warpper) => {
               label: t.get('Version') + ' 4',
               value: 'v4',
             },
-            {
-              label: t.get('Version') + ' 4 (UI v2)',
-              value: 'v4_v2',
-            },
+            // {
+            //   label: t.get('Version') + ' 4 (UI v2)',
+            //   value: 'v4_v2',
+            // },
             // {
             //   label: t.get('Version') + ' 5 (beta)',
             //   value: 'v5',
@@ -48,10 +48,10 @@ export const ProModeWindow_warpper = (props: IProModeWindow_warpper) => {
         />
       </div>
 
-      {/* {version === 'v5' && <ProModeWindow_v5 t={t} locale={locale} />} */}
-      {version === 'v4' && <ProModeWindow_v4 t={t} locale={locale} />}
-      {version === 'v4_v2' && <ProModeWindow_v4_v2 t={t} />}
       {version === 'v3' && <ProModeWindow_v3 t={t} />}
+      {version === 'v4' && <ProModeWindow_v4 t={t} locale={locale} />}
+      {/* {version === 'v4_v2' && <ProModeWindow_v4_v2 t={t} />} */}
+      {/* {version === 'v5' && <ProModeWindow_v5 t={t} locale={locale} />} */}
     </div>
   );
 };
