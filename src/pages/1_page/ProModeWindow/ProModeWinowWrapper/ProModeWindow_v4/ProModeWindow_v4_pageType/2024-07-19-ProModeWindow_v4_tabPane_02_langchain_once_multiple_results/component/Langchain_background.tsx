e623614614a2ce8a4729d202920f3f2c74c07b53
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-10-07 22:46:57 +0200

    Update codes

--- src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2024-07-19-ProModeWindow_v4_tabPane_02_langchain_once_multiple_results/component/Langchain_background.tsx
+++ src/pages/1_page/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2024-07-19-ProModeWindow_v4_tabPane_02_langchain_once_multiple_results/component/Langchain_background.tsx
@@ -4,7 +4,7 @@ import { EyeOutlined, EyeInvisibleOutlined, InfoCircleOutlined } from '@ant-desi
 
 import { IGetT_frontend_output } from '../../../../../../../../gpt-ai-flow-common/i18nProvider/ILocalesFactory';
 import {
-  IPromode_v4_tabPane_context_for_type_langchain_formItems,
+  IProMode_v4_tabPane_context_for_type_langchain_formItems,
   IBackground_for_type_langchain,
   IFormItem,
 } from '../../../../../../../../gpt-ai-flow-common/interface-app/1_page/IProMode_v4/interface-type/03-langchain';
@@ -14,7 +14,7 @@ const { TextArea } = Input;
 
 export const Langchain_background = (props: {
   t: IGetT_frontend_output;
-  backgroundSelected: IPromode_v4_tabPane_context_for_type_langchain_formItems<IBackground_for_type_langchain>;
+  backgroundSelected: IProMode_v4_tabPane_context_for_type_langchain_formItems<IBackground_for_type_langchain>;
   background: IBackground_for_type_langchain;
   setBackground: (newItem: IBackground_for_type_langchain) => void;
 }) => {
