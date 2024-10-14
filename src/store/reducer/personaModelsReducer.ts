e64a03df6eeee6d591949b13e5b2226995c14364
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-10-14 22:25:22 +0200

    Create src/store/actions/personaModelsAction.ts

--- src/store/reducer/personaModelsReducer.ts
+++ src/store/reducer/personaModelsReducer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 import { IPersonaModel } from '../../gpt-ai-flow-common/interface-app/2_component/IPersonaModel/IPersonaModel_index';
 import { IAction } from '../store';
 
@@ -12,6 +13,19 @@ const initialState: IPersonaModelsReducerState = {
 export const personaModelsReducer = (state: IPersonaModelsReducerState = initialState, action: IAction) => {
   const { type, payload } = action;
   switch (type) {
+    case 'ADD_PERSONA_MODEL':
+      const newPersonaModel = { ...payload };
+      return { user: [...state.user, newPersonaModel] };
+    case 'EDIT_PERSONA_MODEL':
+      const updatedPersonaModel = { ...payload };
+      return {
+        user: state.user.map((personaModel: IPersonaModel) =>
+          personaModel.uuid === payload.uuid ? { ...personaModel, ...updatedPersonaModel } : personaModel,
+        ),
+      };
+    case 'DELETE_PERSONA_MODEL':
+      const deletedPersonaModelId = payload;
+      return { user: state.user.filter((personaModel: IPersonaModel) => personaModel.uuid !== deletedPersonaModelId) };
     default:
       return state;
   }
