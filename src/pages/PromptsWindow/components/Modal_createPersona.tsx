825458844bd63cf919b53e144b6e50cbfa62b89f
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-10-19 20:15:33 +0200

    Update

--- src/pages/PromptsWindow/components/Modal_createPersona.tsx
+++ src/pages/PromptsWindow/components/Modal_createPersona.tsx
@@ -32,14 +32,14 @@ export const Drawer_createPersona = (props: IDrawer_createPersona_input) => {
     }
 
     // Calculate newValue
-    const newValue = `
-      ${t.get('Occupation')}: ${occupation}
-      ${t.get('Core values')}: ${coreValues}
-      ${t.get('Unique skills')}: ${uniqueSkill}
-      ${t.get('Personality traits')}: ${personalityTrait}
-      ${t.get('Appearance')}: ${appearance}
-      ${t.get('Additional information')}: ${additionalInfo}
-    `;
+    let newValue = '"""';
+    if (occupation) newValue += `${t.get('Occupation')}: ${occupation}\n`;
+    if (coreValues) newValue += `${t.get('Core values')}: ${coreValues}\n`;
+    if (uniqueSkill) newValue += `${t.get('Unique skills')}: ${uniqueSkill}\n`;
+    if (personalityTrait) newValue += `${t.get('Personality traits')}: ${personalityTrait}\n`;
+    if (appearance) newValue += `${t.get('Appearance')}: ${appearance}\n`;
+    if (additionalInfo) newValue += `${t.get('Additional information')}: ${additionalInfo}\n`;
+    newValue += '"""';
 
     const createPrompt_v3_modal_values: IPrompt_v3_IPersonaModel = createPrompt_v3_form.getFieldsValue();
     const newPrompts_v3 = {
--- src/pages/PromptsWindow/components/Modal_editPersona.tsx
+++ src/pages/PromptsWindow/components/Modal_editPersona.tsx
@@ -35,14 +35,14 @@ export const Drawer_editPersona = (props: IModal_editPersona_input) => {
     }
 
     // Calculate newValue
-    const newValue = `
-    ${t.get('Occupation')}: ${occupation}
-    ${t.get('Core values')}: ${coreValues}
-    ${t.get('Unique skills')}: ${uniqueSkill}
-    ${t.get('Personality traits')}: ${personalityTrait}
-    ${t.get('Appearance')}: ${appearance}
-    ${t.get('Additional information')}: ${additionalInfo}
-  `;
+    let newValue = '"""';
+    if (occupation) newValue += `${t.get('Occupation')}: ${occupation}\n`;
+    if (coreValues) newValue += `${t.get('Core values')}: ${coreValues}\n`;
+    if (uniqueSkill) newValue += `${t.get('Unique skills')}: ${uniqueSkill}\n`;
+    if (personalityTrait) newValue += `${t.get('Personality traits')}: ${personalityTrait}\n`;
+    if (appearance) newValue += `${t.get('Appearance')}: ${appearance}\n`;
+    if (additionalInfo) newValue += `${t.get('Additional information')}: ${additionalInfo}\n`;
+    newValue += '"""';
 
     const createPrompt_v3_modal_values: IPrompt_v3_IPersonaModel = editPrompt_v3_from.getFieldsValue();
     const newPrompts_v3 = {
