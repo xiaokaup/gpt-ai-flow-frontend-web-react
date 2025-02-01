5e2d8913f51a645af8f4264b7e77da7ba5bf3559
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-02-01 18:50:30 +0100

    Update codes

--- src/pages/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2025-01-31-ProModeWindow_v4_subVersion_2_tabPane_02_langchain_once_multiple_results/index_v5.tsx
+++ src/pages/ProModeWindow/ProModeWinowWrapper/ProModeWindow_v4/ProModeWindow_v4_pageType/2025-01-31-ProModeWindow_v4_subVersion_2_tabPane_02_langchain_once_multiple_results/index_v5.tsx
@@ -78,9 +78,10 @@ export const ProModeWindow_v4_subVersion_2_tabPane_02_langchain_once_multiple_re
 
   // Manage multiple outputs results
   const [messagesOutputs_num, setMessagesOutputs_num] = useState<number>(
-    Object.keys(inputsCache_v3).includes(contextSelected_uuid)
-      ? inputsCache_v3[contextSelected_uuid]?.messagesOutputs_num
-      : IInputsCache_v3_contextSelected_value_default.messagesOutputs_num,
+    // Object.keys(inputsCache_v3).includes(contextSelected_uuid)
+    //   ? inputsCache_v3[contextSelected_uuid]?.messagesOutputs_num
+    //   : IInputsCache_v3_contextSelected_value_default.messagesOutputs_num,
+    1,
   );
   const [messages_outputs, setMessages_outputs] = useState<IChatMessage[]>([]);
 
