440aaaf6878c8bec071c1a0363ed6a2211863fb7
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-04-17 18:02:08 +0200

    aa

--- src/pages/settingsWindow/settingsWindow_4_payment/SettingsWindow_4_payment_freeEdition.tsx
+++ src/pages/settingsWindow/settingsWindow_4_payment/SettingsWindow_4_payment_freeEdition.tsx
@@ -31,6 +31,7 @@ export const SettingsWindow_4_payment_freeEdition = (props: ISettingsWindow_4_pa
       trial_period_days?: number;
     },
   ) => {
+    console.log('subscriptionData', subscriptionData);
     const checkoutSessionResults = await TBackendStripeFile.createStripeCheckoutSession_v3(
       {
         userId,
