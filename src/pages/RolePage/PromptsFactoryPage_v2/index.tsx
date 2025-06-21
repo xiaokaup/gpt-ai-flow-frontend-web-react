9799650214940053509cd76efef66467f9b37d00
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-06-21 19:11:03 +0800

    Add back onDragOver

--- src/pages/RolePage/PromptsFactoryPage_v2/index.tsx
+++ src/pages/RolePage/PromptsFactoryPage_v2/index.tsx
@@ -126,7 +126,7 @@ export const PromptsFactoryPage_v2 = (props: IPromptsFactoryPage) => {
           <DndContext
             sensors={useSensors(useSensor(PointerSensor))}
             collisionDetection={closestCenter}
-            // onDragOver={handleDragEnd}
+            onDragOver={handleDragEnd}
             onDragEnd={handleDragEnd}
           >
             <div className="flex flex-col gap-8 mt-4 pr-2">
