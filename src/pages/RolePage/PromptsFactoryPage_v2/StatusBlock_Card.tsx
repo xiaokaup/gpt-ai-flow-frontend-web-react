6ee793f8216a8bf9d5b39ba087b328119db46e9d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-06-21 23:21:22 +0800

    Add UI to edit

--- src/pages/RolePage/PromptsFactoryPage_v2/StatusBlock_Card.tsx
+++ src/pages/RolePage/PromptsFactoryPage_v2/StatusBlock_Card.tsx
@@ -1,6 +1,7 @@
 import { CSS } from '@dnd-kit/utilities';
-import { IPrompt_v3_for_promptsFactory } from '../../../gpt-ai-flow-common/interface-app/3_unit/IPrompt_v3_for_promptsFactory';
 import { useSortable } from '@dnd-kit/sortable';
+import { EditOutlined } from '@ant-design/icons';
+import { IPrompt_v3_for_promptsFactory } from '../../../gpt-ai-flow-common/interface-app/3_unit/IPrompt_v3_for_promptsFactory';
 
 interface IStatusBlock_Card {
   view: 'simple' | 'advanced';
@@ -8,7 +9,7 @@ interface IStatusBlock_Card {
 }
 export const StatusBlock_Card = (props: IStatusBlock_Card) => {
   const { view, onePrompt } = props;
-  const { title: id, title: title, content } = onePrompt;
+  const { title: id, title, content } = onePrompt;
 
   // 使用 useSortable 处理排序
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
@@ -35,7 +36,12 @@ export const StatusBlock_Card = (props: IStatusBlock_Card) => {
       style={style}
     >
       <div className="flex justify-between items-center">
-        <h3 className="font-medium text-neutral-100">{title}</h3>
+        <div className="w-full flex justify-between items-center">
+          <h3 className="font-medium text-neutral-100">{title}</h3>
+          <div className="font-medium text-neutral-100">
+            <EditOutlined className="cursor-pointer" />
+          </div>
+        </div>
       </div>
       {view === 'advanced' && <p className="mt-2 text-sm text-neutral-400 max-h-[200px] overflow-auto">{content}</p>}
     </div>
