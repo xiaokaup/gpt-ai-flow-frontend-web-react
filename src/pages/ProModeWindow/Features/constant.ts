ce1a3cd1a4c5047f041f777bc651e9464d2be805
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-09-01 18:03:59 +0800

    Optimize codes

--- src/pages/ProModeWindow/Features/constant.ts
+++ src/pages/ProModeWindow/Features/constant.ts
@@ -1,5 +1,6 @@
-import { isProd } from '../../../gpt-ai-flow-common/config/constantGptAiFlow';
+import { CONSTANTS_GPT_AI_FLOW_COMMON } from '../../../gpt-ai-flow-common/config/constantGptAiFlow';
 
-export const webAppUrl = isProd ? 'https://www.app.gptaiflow.com' : 'http://localhost:3000';
+// export const webAppUrl = isProd ? 'https://www.app.gptaiflow.com' : 'http://localhost:3000';
+export const webAppUrl = CONSTANTS_GPT_AI_FLOW_COMMON.FRONTEND_WEB_ENDPOINT;
 // const docBaseUrl = isProd ? 'https://www.gptaiflow.com' : 'http://localhost:3002';
 export const docBaseUrl = 'https://www.gptaiflow.com';
